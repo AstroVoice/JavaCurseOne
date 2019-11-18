83428e5bfc8b4e0d910a85f7cb1fb933d7bcd1c4
AstroVoice <alexander.strugovshchikov@gmail.com>
2019-11-18 16:48:36 +0300

    ДЗ 6 - Дополняется

--- src/Lesson6/startHere.java
+++ src/Lesson6/startHere.java
@@ -1,4 +1,30 @@
 package Lesson6;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class startHere {
+
+//    Создать 2 текстовых файла, примерно по 50-100 символов в каждом(особого значения не имеет);
+//    Написать программу, «склеивающую» эти файлы, то есть вначале идет текст из первого файла, потом текст из второго.
+//    Написать программу, которая проверяет присутствует ли указанное пользователем слово (или словосочетание, или предложение) в файле. (Работаем только с латиницей)
+//    ** Написать метод, проверяющий, есть ли указанное слово в файлах папки
+
+    public static void main(String[] args) {
+        try {
+            InputStream in;
+            OutputStream out;
+            FileInputStream fin = new FileInputStream("./src/Lesson6/file1.txt");
+            System.out.println(fin.read());
+            fin.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 }
